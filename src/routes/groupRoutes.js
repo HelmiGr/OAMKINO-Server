e2ed9857df79563954a20aa3874ce26520b409e8
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-12-12 18:57:57 +0200

    ** add new message table in webapp, add auto test browsing review

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const { authToken } = require('../../config/auth');
-const { createGroup, listAllGroups, getGroupDetails, deleteGroup } = require('../controllers/groupController');
+const express = require("express");
+const { authToken } = require("../../config/auth");
+const {
+  createGroup,
+  listAllGroups,
+  getGroupDetails,
+  deleteGroup,
+} = require("../controllers/groupController");
 const groupRouter = express.Router();
 
 // POST to create a group
-groupRouter.post('/', authToken, createGroup);
+groupRouter.post("/", authToken, createGroup);
 
 // GET to get groups
-groupRouter.get('/all', listAllGroups);
+groupRouter.get("/all", listAllGroups);
 
 // GET to get details of a group
-groupRouter.get('/all/:groupId', getGroupDetails);
+groupRouter.get("/all/:groupId", getGroupDetails);
 
 // DELETE to delete a group
-groupRouter.delete('/delete/:id', authToken, deleteGroup);
-
+groupRouter.delete("/delete/:id", authToken, deleteGroup);
 
 module.exports = groupRouter;
